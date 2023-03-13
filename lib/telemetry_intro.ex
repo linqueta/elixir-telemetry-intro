e41039694a8bc5f7150eafb30e4295ca20269bb0
linqueta <lincolnrodrs@gmail.com>
2023-03-13 11:20:59 -0300

    Send metric after sale

--- lib/telemetry_intro.ex
+++ lib/telemetry_intro.ex
@@ -1,18 +1,10 @@
 defmodule TelemetryIntro do
-  @moduledoc """
-  Documentation for `TelemetryIntro`.
-  """
+  use Application
 
-  @doc """
-  Hello world.
+  def start(_type, _args) do
+    TelemetryIntro.Metrics.Instrumenter.setup()
 
-  ## Examples
-
-      iex> TelemetryIntro.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    children = []
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
